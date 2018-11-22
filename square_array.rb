0e37d58e7c38df5d16fd62430d2bc40124119dc6
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 19:48:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 numbers = [1,2,3]
-arr = []
+
 
 def square_array(numbers)
-    numbers.each! do {|i| 
-        arr << i ** 2
-        arr
-    end
+    arr = []
+    numbers.each! do {|i| arr << i ** 2}
+    arr
+  end
   
 end
