f4306866acd6e9dd6fb63360f7e111ed83356bf6
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 19:45:04 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+numbers = [1,2,3]
+arr = []
+
+def square_array(numbers)
+    numbers.each do |i| 
+        arr << i ** 2
+        arr
+    end
+  
 end
