98ef21b6c603b3498be7568a59c3e6e72810c15f
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 19:58:29 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,8 +3,9 @@ numbers = [1,2,3]
 
 def square_array(numbers)
     arr = []
-      numbers.each do { |i| arr << i ** 2 }
-    arr
+      numbers.each do |i|
+        num = i ** 2
+        arr << num
   end
-  
+  arr
 end
